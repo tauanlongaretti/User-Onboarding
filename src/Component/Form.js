c88a44ca1e6e3c513413e9fffe9a844a74f57156
tauan-longaretti <tauanlongaretti@gmail.com>
2019-12-12 23:21:15 -0500

    created formik form with inputs for name, email, password, TOS and submit button

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, {useState, Component} from "react";
+import { withFormik, Field, Form } from "formik"
+import { __values } from "tslib";
 
-function Form () {
-  return ("Hello, I am the form component!")
+const TheForm = ({values}) => {
+  return (
+      <Form>
+        <label htmlFor="name">
+            Name:
+            <Field type="text" name="name" value={values.name} />
+        </label>
+        <label htmlFor="email">
+            Email:
+            <Field type="text" name="email" value={values.email} />
+        </label>
+        <label htmlFor="password">
+            Password:
+            <Field type="text" name="password" value={values.password} />
+        </label>
+        <label htmlFor="TOS" >
+            Terms of Service
+            <Field type="checkbox" name="TOS" checked={values.TOS} />
+        </label>
+        <button type="submit">Submit</button>
+      </Form>
+  )
 }
 
-export default Form;
+const FormikForm = withFormik({
+    mapPropsToValues({name, email, password, TOS}) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            TOS: TOS || false,
+        };
+    }
+})(TheForm);
+
+export default FormikForm;
